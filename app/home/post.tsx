fd044e7c777aeb59d7aa66bdb53cfd32948f9c98
Richard <richarddavidrv13@gmail.com>
2025-03-05 22:00:07 -0400

    cambios posts

--- app/home/post.tsx
+++ app/home/post.tsx
@@ -2,7 +2,7 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
-import { Heart } from "lucide-react";
+import { Heart, MessageCircle, Send } from "lucide-react";
 import { useState } from "react";
 
 export default function Home() {
@@ -25,12 +25,18 @@ export default function Home() {
                     </div>
                 </CardTitle>
             </CardHeader>
-            <CardContent>
+            <CardContent className="pb-2">
                 <img className="w-full" src="https://github.com/shadcn.png" alt="@shadcn" />
             </CardContent>
             <CardFooter>
-                <div>
-                    {like ? <Heart onClick={changeLike} fill="red"  strokeWidth={2} stroke="red"/> : <Heart onClick={changeLike} strokeWidth={2}/>}
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-row gap-2">
+                        {like ? <Heart className="cursor-pointer" onClick={changeLike} fill="red"  strokeWidth={2} stroke="red"/> : <Heart className="cursor-pointer" onClick={changeLike} strokeWidth={2}/>}
+                        <span className="font-semibold ml-[-5px]">20</span>
+                        <MessageCircle className="cursor-pointer" strokeWidth={2} />
+                        <Send className="mt-[1px] cursor-pointer" strokeWidth={2} />
+                    </div>
+                    <span><strong>User123</strong> La vida es waos</span>
                 </div>
             </CardFooter>
         </Card>
